2020768949833aef4b131bd7fbd989b53330ebb5
perjinshavani <persha2008@hotmail.com>
2026-08-07 15:27:29 +0200

    Fix page.tsx after merge with main

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import ProductTable from "./components/ProductTable";
 import DashboardHeader from "./components/DashboardHeader";
 import DashboardCards from "./components/DashboardCards";
 
+
+
 const API_URL = "http://localhost:4000";
 const defaultLimit = "6";
 
@@ -16,16 +18,29 @@ export default async function Home() {
     `${API_URL}/products/`,
   ).then((res) => res.json());
 
+
+  const categoriesResponse = await fetch(
+          `${API_URL}/categories/`);
+  
+  if (!categoriesResponse.ok) {
+    throw new Error("Failed to fetch categories");
+  }
+  
+  const categories = await categoriesResponse.json();
+
+    
+  
+    
+      
+  
+
   return (
     <main>
-      
-    
-      <DashboardHeader  />
+      <h1>Products</h1>
+      <DashboardHeader />
       <DashboardCards />
-       <SearchField categories={categories} />
-        
-      <ProductTable products={products} />
-      
+      <SearchField categories={categories} />  
+      <ProductTable products={products} />    
       
       
     </main>
