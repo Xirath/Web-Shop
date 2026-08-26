f1b302ffb4a4e559590849f1371b19fc68baf8d8
perjinshavani <persha2008@hotmail.com>
2026-08-26 16:57:52 +0200

    Improve form field accessibility

--- app/components/AddProductForm.tsx
+++ app/components/AddProductForm.tsx
@@ -31,11 +31,15 @@ export default function AddProductForm({
       />
 
       <div className="mb-4">
-        <label className="mb-1 block text-sm font-medium text-gray-700">
+        <label
+          htmlFor="categoryId"
+          className="mb-1 block text-sm font-medium text-gray-700"
+        >
           Category
         </label>
 
         <select
+          id="categoryId"
           name="categoryId"
           className="rounded-md border border-gray-300 px-3 py-2 text-gray-700"
           required
@@ -65,10 +69,14 @@ export default function AddProductForm({
         required
       />
       <div className="mb-4">
-        <label className="mb-1 block text-sm font-medium text-gray-700">
+        <label
+          htmlFor="description"
+          className="mb-1 block text-sm font-medium text-gray-700"
+        >
           Description
         </label>
         <textarea
+          id="description"
           name="description"
           placeholder="Enter product description"
           className="min-h-28 w-full rounded-md border border-gray-300 px-3 py-2"
--- app/components/FormField.tsx
+++ app/components/FormField.tsx
@@ -13,12 +13,16 @@ export default function FormField({
   placeholder,
   required,
 }: FormFieldProps) {
-  
+
   return (
     <div className="mb-4">
-        <label className="mb-1 block text-sm font-medium text-gray-700">{label}</label>
+        <label
+         htmlFor={name}
+         className="mb-1 block text-sm font-medium text-gray-700">{label}</label>
 
-      <input type={type} 
+      <input
+        id={name} 
+      type={type} 
       name={name}
       placeholder={placeholder}    className="w-full rounded-md border border-gray-300 px-3 py-2"
     required={required}/>
