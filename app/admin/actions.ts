8910b2d357328a43a89bb2e9ea60be5be7d4a905
Daniel Östberg <danielostberg@hotmail.com>
2026-08-26 17:28:06 +0200

    Added edit page and a wrapper for the API calls.

--- app/admin/actions.ts
+++ app/admin/actions.ts
@@ -1,5 +1,48 @@
 "use server";
 
+import { api } from "@/lib/api";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
 export async function deleteProductAction(id: number) {
   // TODO: Implement the delete logic here
+}
+
+export async function editProductAction(id: number, formData: FormData) {
+  console.log("???");
+  const title = formData.get("title") as string;
+  const brand = formData.get("brand") as string;
+  const categoryId = formData.get("categoryId") as string;
+  const price = formData.get("price") as string;
+  const stock = formData.get("stock") as string;
+  const description = formData.get("description") as string;
+  const thumbnail = formData.get("thumbnail") as string;
+
+  console.log("Getting Form Data:", {
+    title,
+    brand,
+  });
+
+  const newProduct = {
+    title,
+    brand,
+    description,
+    thumbnail,
+    categoryId: parseInt(categoryId, 10),
+    price: parseInt(price, 10),
+    stock: parseInt(stock, 10),
+    meta: {
+      updatedAt: new Date().toISOString(),
+    },
+  };
+
+  try {
+    await api.editProduct(id.toString(), newProduct);
+    revalidatePath("/");
+  } catch {
+    console.error("Failed to update product");
+  }
+
+  // TODO: show a success message and redirect to the product list page after successful update
+  //redirect("/admin/?status=success");
 }
--- app/components/FormField.tsx
+++ app/components/FormField.tsx
@@ -1,23 +1,34 @@
 interface FormFieldProps {
+  name: string;
   label: string;
-  type: string;
+  type: "text" | "number";
   placeholder: string;
-   required?: boolean;
+  defaultValue?: string | number;
+  required?: boolean;
 }
 
 export default function FormField({
+  name,
   label,
   type,
   placeholder,
+  defaultValue,
   required,
 }: FormFieldProps) {
   return (
     <div className="mb-4">
-        <label className="mb-1 block text-sm font-medium text-gray-700">{label}</label>
+      <label className="mb-1 block text-sm font-medium text-gray-700">
+        {label}
+      </label>
 
-      <input type={type} 
-      placeholder={placeholder}    className="w-full rounded-md border border-gray-300 px-3 py-2"
-    required={required}/>
+      <input
+        name={name}
+        type={type}
+        placeholder={placeholder}
+        defaultValue={defaultValue}
+        className="w-full rounded-md border border-gray-300 px-3 py-2"
+        required={required}
+      />
     </div>
   );
 }
