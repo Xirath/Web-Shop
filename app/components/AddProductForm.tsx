608fcf00b2059bff53890397acbacc8c3550129b
perjinshavani <persha2008@hotmail.com>
2026-08-26 16:22:40 +0200

    Add create product functionality

--- app/components/AddProductForm.tsx
+++ app/components/AddProductForm.tsx
@@ -1,77 +1,95 @@
-import FormField from "./FormField"
+import { createProductAction } from "@/app/admin/actions";
+import FormField from "./FormField";
 import type { Category } from "../types";
 
-export default function AddProductForm({ categories }: { categories: Category[] }) {
+export default function AddProductForm({
+  categories,
+}: {
+  categories: Category[];
+}) {
   return (
-    <form className="mx-auto max-w-2xl rounded-lg border border-gray-200 bg-white p-6 shadow-sm">
-        
-      <h2 className="mb-6 text-2xl font-bold">
-  Add Product
-</h2>
+    <form
+      action={createProductAction}
+      className="mx-auto max-w-2xl rounded-lg border border-gray-200 bg-white p-6 shadow-sm"
+    >
+      <h2 className="mb-6 text-2xl font-bold">Add Product</h2>
 
       <FormField
         label="Product title"
+        name="title"
         type="text"
         placeholder="Enter product title"
         required
       />
 
       <FormField
-  label="Brand"
-  type="text"
-  placeholder="Enter product brand"
-  required
-/>
+        label="Brand"
+        name="brand"
+        type="text"
+        placeholder="Enter product brand"
+        required
+      />
 
-<div className="mb-4">
-  <label className="mb-1 block text-sm font-medium text-gray-700">
-    Category</label>
+      <div className="mb-4">
+        <label className="mb-1 block text-sm font-medium text-gray-700">
+          Category
+        </label>
 
-  <select className="rounded-md border border-gray-300 px-3 py-2 text-gray-700" required>
-    
-    <option value="">Choose a category</option>
-    {categories.map((category) => (
-      <option key={category.id} value={category.id}>
-        {category.name}
-      </option>
-    ))}
-  </select>
-</div>
+        <select
+          name="categoryId"
+          className="rounded-md border border-gray-300 px-3 py-2 text-gray-700"
+          required
+        >
+          <option value="">Choose a category</option>
+          {categories.map((category) => (
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
 
+      <FormField
+        label="Price"
+        name="price"
+        type="number"
+        placeholder="Enter product price"
+        required
+      />
 
- <FormField
-  label="Price"
-  type="number"
-  placeholder="Enter product price"
-  required
-/>
+      <FormField
+        label="Stock"
+        name="stock"
+        type="number"
+        placeholder="Enter Stock"
+        required
+      />
+      <div className="mb-4">
+        <label className="mb-1 block text-sm font-medium text-gray-700">
+          Description
+        </label>
+        <textarea
+          name="description"
+          placeholder="Enter product description"
+          className="min-h-28 w-full rounded-md border border-gray-300 px-3 py-2"
+          required
+        />
+      </div>
 
- <FormField
-  label="Stock"
-  type="number"
-  placeholder="Enter Stock"
-  required
-/>
-<div className="mb-4">
-  <label className="mb-1 block text-sm font-medium text-gray-700">
-    Description</label>
-  <textarea placeholder="Enter product description" className="min-h-28 w-full rounded-md border border-gray-300 px-3 py-2"
-required />
-</div>
+      <FormField
+        label="Thumbnail"
+        name="thumbnail"
+        type="url"
+        placeholder="Enter image URL"
+        required
+      />
 
-<FormField
-  label="Thumbnail"
-  name="thumbnail"
-  type="url"
-  placeholder="Enter image URL"
-  required
-/>
-
-
-<button
-  type="submit"
-  className="rounded-md bg-blue-600 px-4 py-2 text-white hover:bg-blue-700"
-  
->
-  Add Product </button>
-   </form> ); }
+      <button
+        type="submit"
+        className="rounded-md bg-blue-600 px-4 py-2 text-white hover:bg-blue-700"
+      >
+        Add Product{" "}
+      </button>
+    </form>
+  );
+}
--- app/components/FormField.tsx
+++ app/components/FormField.tsx
@@ -1,5 +1,6 @@
 interface FormFieldProps {
   label: string;
+  name:string;
   type: string;
   placeholder: string;
    required?: boolean;
@@ -7,15 +8,18 @@ interface FormFieldProps {
 
 export default function FormField({
   label,
+  name,
   type,
   placeholder,
   required,
 }: FormFieldProps) {
+  
   return (
     <div className="mb-4">
         <label className="mb-1 block text-sm font-medium text-gray-700">{label}</label>
 
       <input type={type} 
+      name={name}
       placeholder={placeholder}    className="w-full rounded-md border border-gray-300 px-3 py-2"
     required={required}/>
     </div>
