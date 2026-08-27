417a928bf215d03e5485d2da41825d4321fd5acc
Daniel Östberg <danielostberg@hotmail.com>
2026-08-27 10:13:18 +0200

    Fix merge-commit miss

--- app/components/FormField.tsx
+++ app/components/FormField.tsx
@@ -28,6 +28,7 @@ export default function FormField({
         id={name}
         type={type}
         name={name}
+        defaultValue={defaultValue}
         placeholder={placeholder}
         className="w-full rounded-md border border-gray-300 px-3 py-2"
         required={required}
